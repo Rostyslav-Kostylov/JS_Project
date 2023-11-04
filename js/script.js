cd36c9a97a222297e8790b285d3b21d944a090b8
Rostyslav Kostylov <rostik.costylyow@gmail.com>
2023-11-04 21:57:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+// const obj = {
+// 	a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+const obj = {
+	name: "Rost",
+	age: 26,
+	isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ["plum.png", "orange.jpg", 6, "apple.bpm", {}, []];
+console.log(arr[2]);
